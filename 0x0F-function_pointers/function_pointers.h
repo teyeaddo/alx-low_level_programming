99d7267e5fab59f962350a44e17c8d37b5a70af2
teyeaddo <addorichmondteye@gmail.com>
2022-04-14 05:21:36 +0000

    New prototypes added

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,14 @@
-#ifndef FUNC_H
-#define FUNC_H
+#ifndef FILE_FUNCTION
+#define FILE_FUNCTION
+#include <stdio.h>
 #include <stdlib.h>
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
+
 
 #endif
 
