a219da8b9b000c301c863d8f2363c8b079ee6bbd
teyeaddo <addorichmondteye@gmail.com>
2022-04-22 10:44:47 +0000

    This adds a new node to the beginning of a list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,9 +3,9 @@
 #include "lists.h"
 
 /**
- * add_node - this adds a new node at the beginning of a linked list
- * @head: a double pointer to the list_t list
- * @str: the new string to add in the node
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
  *
  * Return: the address of the new element, or NULL if it fails
  */
